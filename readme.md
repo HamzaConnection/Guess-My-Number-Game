76347cfda509067ea2f913ca93ee3ee0f7d9da22
Hamza Laroussi <HamzaConnection@hotmail.com>
2021-01-29 20:34:26 +0100

    finished guess my number game wuhu!

---
+++ readme.md
@@ -0,0 +1,5 @@
+I made this myself only using javascript no frameworks/libraries the html and css I got from somewhere else all the JS is created by me.
+
+The game can be played here:
+
+# url
---
+++ script.js
@@ -0,0 +1,122 @@
+"use strict";
+
+const correctAnswer = (() => {
+  return Math.floor(Math.random() * 20) + 1;
+})();
+
+let getGuess = () => {
+  return Number(document.querySelector(".guess").value);
+};
+
+console.log(correctAnswer);
+
+document.querySelector(".check").addEventListener("click", () => {
+  if (youWon()) {
+    won();
+  } else {
+    youMissed(getGuess());
+    if (youLost()) {
+      lost();
+    }
+  }
+});
+
+// YOU MISSED
+let youMissed = (guess) => {
+  let wrongAnswerMessage = "Wrong Answer Try again";
+  lossPoints();
+  if (guess > correctAnswer) {
+    setMessage("Try a lower number ⬇️");
+  } else if (guess < correctAnswer) {
+    setMessage("Try a higher number ⬆️");
+  }
+};
+
+// YOU WON
+let youWon = () => {
+  return getGuess() === correctAnswer;
+};
+
+let won = () => {
+  let winingMessage = "CONGRATULATION YOU WON!🎉";
+  document.body.style.backgroundColor = "#60b347";
+  setHighscore(getScore());
+  showCorrectAnswer();
+  setMessage(winingMessage);
+  disableGuessing();
+};
+
+// YOU LOST
+
+let youLost = () => {
+  return getScore() === 0;
+};
+let lost = () => {
+  let lossingMessage = "YOU LOST!😞";
+  document.body.style.backgroundColor = "#DC143C";
+  setMessage(lossingMessage);
+  disableGuessing();
+};
+
+// SHOW/HIDE CORRECT ANSWER
+let showCorrectAnswer = () => {
+  document.querySelector(".number").textContent = correctAnswer;
+};
+
+let hideCorrectAnswer = () => {
+  document.querySelector(".number").textContent = "?";
+};
+
+// TRY AGAIN
+document.querySelector(".again").addEventListener("click", () => {
+  tryAgain();
+});
+
+let tryAgain = () => {
+  setHighscore(getHighscore());
+  resetScore(20);
+  hideCorrectAnswer();
+  setMessage("Start guessing...");
+  document.body.style.backgroundColor = "#222";
+  enableGuessing();
+};
+
+// SET HighScore
+let setHighscore = (currentScore) => {
+  document.querySelector(".highscore").textContent = currentScore;
+};
+
+let getHighscore = (currentScore) => {
+  return Number(document.querySelector(".highscore").textContent);
+};
+
+let getScore = () => {
+  return Number(document.querySelector(".score").textContent);
+};
+
+let resetScore = (value) => {
+  document.querySelector(".score").textContent = 20;
+};
+
+let lossPoints = () => {
+  return (document.querySelector(".score").textContent = getScore() - 2);
+};
+
+// GET SET MESSAGE
+let getMessage = () => {
+  return document.querySelector(".message").textContent;
+};
+
+let setMessage = (value) => {
+  document.querySelector(".message").textContent = value;
+};
+
+// ENABLE/ DISABLE GUESSING
+
+let disableGuessing = () => {
+  document.querySelector(".check").disabled = true;
+};
+
+let enableGuessing = () => {
+  document.querySelector(".check").disabled = false;
+};
